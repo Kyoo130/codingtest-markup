e89422ebef3411f105eb4d3049eee3d3d965878f
Kyoo130 <kml9003@kakao.com>
2021-11-10 17:35:04 +0900

    [update] uploadImg js commit

--- 3dCarousel/src/js/main.js
+++ 3dCarousel/src/js/main.js
@@ -1,5 +1,5 @@
 (() => {
-    const carouselUI = document.querySelector(".carousel-list");
+    const carouselUl = document.querySelector(".carousel-list");
     const imageInput = document.querySelector("#image-upload-input");
     const nextButton = document.querySelector(".next-btn");
     const prevButton = document.querySelector(".prev-btn");
@@ -11,7 +11,7 @@
             const currentItem = document.querySelector(".carousel-front");
             const next = currentItem.nextElementSibling;
 
-            carouselUI.appendChild(currentItem);
+            carouselUl.appendChild(currentItem);
             currentItem.classList.remove('carousel-front');
             next.classList.add('carousel-front')
         }
@@ -22,14 +22,49 @@
 
         if (items.length > 1) {
             const currentItem = document.querySelector(".carousel-front");
-            const lastItem = carouselUI.lastElementChild;
-            
-            carouselUI.insertBefore(lastItem, items[0]);
+            const lastItem = carouselUl.lastElementChild;
+
+            carouselUl.insertBefore(lastItem, items[0]);
             currentItem.classList.remove('carousel-front');
             lastItem.classList.add('carousel-front');
         }
     }
 
+    function createTag(url) {
+        const list = document.createElement('li');
+        const img = document.createElement('img');
+        list.classList.add("carousel-item");
+        img.src = url;
+        list.appendChild(img);
+
+        const items = document.querySelectorAll(".carousel-item");
+        items.forEach(item => {
+            item.classList.remove('carousel-front')
+        });
+        list.classList.add('carousel-front');
+
+        return list;
+    }
+
+    function uploadImg(value) {
+        const items = document.querySelectorAll(".carousel-item");
+
+        if (value.files) {
+            const reader = new FileReader();
+
+            reader.onload = e => {
+                const imgUrl = e.target.result;
+                carouselUl.insertBefore(createTag(imgUrl), items[0]);
+            }
+
+            reader.readAsDataURL(value.files[0]);
+        }
+    }
+
+    imageInput.addEventListener("change", e => {
+        uploadImg(e.target);
+
+    })
     nextButton.addEventListener("click", moveNext);
     prevButton.addEventListener("click", moveprev);
 })();
