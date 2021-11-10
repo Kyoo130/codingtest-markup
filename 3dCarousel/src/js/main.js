f7f3dd34c987dc2ae10fda8389d90a531c63849c
Kyoo130 <kml9003@kakao.com>
2021-11-10 22:34:35 +0900

    [update] card translate commit

--- 3dCarousel/src/js/main.js
+++ 3dCarousel/src/js/main.js
@@ -4,6 +4,38 @@
     const nextButton = document.querySelector(".next-btn");
     const prevButton = document.querySelector(".prev-btn");
 
+    function changeTransform() {
+        const items = document.querySelectorAll(".carousel-item");
+
+        items.forEach((e, i) => {
+            let degree = 360 / items.length;
+
+            if (items.length > 1) {
+                if (i == 0) {
+                    e.style.transform = "rotateY(0deg) translateZ(250px)";
+                } else {
+                    e.style.transform = `rotateY(${degree * i}deg) translateZ(250px) rotateY(-${degree * i}deg)`
+                }
+            }
+
+            if (items.length >= 5) {
+                if (i == 0) {
+                    e.style.transform = "rotateY(0deg) translateZ(250px)";
+                } else if (i == 1) {
+                    e.style.transform = "rotateY(72deg) translateZ(250px) rotateY(-72deg)";
+                } else if (i == 2) {
+                    e.style.transform = "rotateY(144deg) translateZ(250px) rotateY(-144deg) translateX(400px)";
+                } else if (i == items.length - 2) {
+                    e.style.transform = "rotateY(216deg) translateZ(250px) rotateY(-216deg) translateX(-400px)";
+                } else if (i == items.length - 1) {
+                    e.style.transform = "rotateY(288deg) translateZ(250px) rotateY(-288deg)";
+                } else {
+                    e.style.transform = `rotateY(${degree * i}deg) translateZ(250px) rotateY(-${degree * i}deg)`
+                }
+            }
+        });
+    }
+
     function moveNext() {
         const items = document.querySelectorAll(".carousel-item");
 
@@ -15,6 +47,7 @@
             currentItem.classList.remove('carousel-front');
             next.classList.add('carousel-front')
         }
+        changeTransform();
     }
 
     function moveprev() {
@@ -28,6 +61,7 @@
             currentItem.classList.remove('carousel-front');
             lastItem.classList.add('carousel-front');
         }
+        changeTransform();
     }
 
     function createTag(url) {
@@ -55,6 +89,7 @@
             reader.onload = e => {
                 const imgUrl = e.target.result;
                 carouselUl.insertBefore(createTag(imgUrl), items[0]);
+                changeTransform();
             }
 
             reader.readAsDataURL(value.files[0]);
@@ -67,4 +102,8 @@
     })
     nextButton.addEventListener("click", moveNext);
     prevButton.addEventListener("click", moveprev);
+
+    window.onload = () => {
+        changeTransform();
+    }
 })();
